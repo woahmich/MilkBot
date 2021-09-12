6e6ccc5b56d0e0210f43559df822eafee4c0e44f
woahmich <89962123+woahmich@users.noreply.github.com>
2021-09-12 12:14:56 -0700

    Added check possible moves for king
    also created kingSafe method which checks if the move puts the king into check, hasn't been finished though. Also does not support castling yet. For some reason if you put multiple kings on the board, it only checks for one of them and then terminates, it doesn't check for all the kings. Also only currently works for the white(upper case) king.

--- milkBotChess.java
+++ milkBotChess.java
@@ -12,12 +12,14 @@ public class milkBotChess {
 	static String chessBoard[][] = {
 					{"r","k","b","q","a","b","k","r"},
 			        {"p","p","p","p","p","p","p","p"},
-			        {" "," "," "," "," "," ","P"," "},
+			        {" "," "," "," "," "," "," "," "},
 			        {" "," "," "," "," "," "," "," "},
 			        {" "," "," "," "," "," "," "," "},
 			        {" "," "," "," "," "," "," "," "},
 			        {"P","P","P","P","P","P","P","P"},
 			        {"R","K","B","Q","A","B","K","R"}};
+	
+	static int kingPositionC, kingPositionL;
 
 	public static void main(String[] args) {
 		
@@ -35,6 +37,7 @@ public class milkBotChess {
 		f.add(ui);
 		f.setSize(500, 500);
 		f.setVisible(true); */
+		System.out.println(possibleMoves());
 		
 
 	}
@@ -60,7 +63,7 @@ public class milkBotChess {
 			}
 		}
 		
-		return ""; // x1,y1, x2, y2, captured piece
+		return list; // x1, y1, x2, y2, captured piece
 	}
 	
 	// adds possible moves to a list for each piece
@@ -81,8 +84,41 @@ public class milkBotChess {
 		String list="";
 		return list; 
 	}public static String possibleA(int i) {
-		String list="";
+		
+		String list="", oldPiece;
+		int r=i/8, c=i%8;
+		for(int j=0; j<9; j++) {
+			if(j!=4) {
+				
+				try {
+
+					if(Character.isLowerCase(chessBoard[r-1+j/3][c-1+j%3].charAt(0)) || " ".equals(chessBoard[r-1+j/3][c-1+j%3])) {
+						oldPiece=chessBoard[r-1+j/3][c-1+j%3];
+						// make king's old position a blank, update king's new position
+						chessBoard[r][c]=" ";
+						chessBoard[r-1+j/3][c-1+j%3]="A";
+						int kingTemp=kingPositionC;
+						kingPositionC=i+(j/3)*8+j%3-9;
+						// check if kings move is legal (does it put king in check?)
+						if(kingSafe()) {
+							list=list+r+c+(r-1+j/3)+(c-1+j%3) + oldPiece;
+						}
+						// return king to original position after checking new one
+						chessBoard[r][c]="A";
+						chessBoard[r-1+j/3][c-1+j%3]=oldPiece;
+						kingPositionC=kingTemp;
+
+					}
+				} catch(Exception e) {}
+			}
+		}
+		// still need to add castling
 		return list; 
+	}
+	
+	// method to check if King is in check
+	public static boolean kingSafe() {
+		return true;
 	}
 	
 }
