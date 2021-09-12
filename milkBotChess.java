f0399bd88e11bae4f43046519b3e802e318b917c
woahmich <89962123+woahmich@users.noreply.github.com>
2021-09-11 23:16:58 -0700

    Creates a window and calls ui

--- milkBotChess.java
+++ milkBotChess.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+
 public class milkBotChess {
-  public static void main(String[] args) {
-  
-  }
+
+	public static void main(String[] args) {
+		
+		// create window for board
+		JFrame f=new JFrame("MilkBot Chess");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// call user interface
+		userInterface ui=new userInterface();
+		
+		// add user interface to window
+		f.add(ui);
+		f.setSize(500, 500);
+		f.setVisible(true);
+		
+
+	}
+
 }
